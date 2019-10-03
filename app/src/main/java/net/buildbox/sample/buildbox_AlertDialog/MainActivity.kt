8239f89c807ed8151b10f968aa5bbbd03f1154b6
Koji Shigemura <shige0501@gmail.com>
2019-10-03 21:28:38 +0900

    AlertDialogの表示

--- app/src/main/java/net/buildbox/sample/buildbox_AlertDialog/MainActivity.kt
+++ app/src/main/java/net/buildbox/sample/buildbox_AlertDialog/MainActivity.kt
@@ -2,11 +2,20 @@ package net.buildbox.sample.buildbox_AlertDialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onShowAlertDialog(view: View) {
+        AlertDialog.Builder(this)
+            .setTitle("ダイアログのタイトルです")
+            .setMessage("ダイアログのメッセージです。伝えたい内容をここに記入してください。")
+            .show()
     }
 }
